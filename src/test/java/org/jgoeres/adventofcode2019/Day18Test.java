2d3312c6e1aa121b345d7083530b24dd3d61fa24
Justin Goeres <jgoeres@liveareacx.com>
2019-12-18 14:18:58 -0500

    WIP Day 18A Add input file loading and examples

--- src/test/java/org/jgoeres/adventofcode2019/Day18Test.java
+++ src/test/java/org/jgoeres/adventofcode2019/Day18Test.java
@@ -1,11 +1,36 @@
 package org.jgoeres.adventofcode2019;
 
+import org.jgoeres.adventofcode2019.Day18.Day18Service;
 import org.jgoeres.adventofcode2019.Day18.RunDay18;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class Day18Test {
     static String DAY = "18";
+
+    @Test
+    public void testDay18AExample1() {
+        Day18Service day18Service = new Day18Service("data/day18/example1.txt");
+        int result = 0;
+        try {
+//            result = day18Service();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void testDay18AExample2() {
+        Day18Service day18Service = new Day18Service("data/day18/example2.txt");
+        int result = 0;
+        try {
+//            result = day18Service();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        Assert.assertEquals(0, result);
+    }
 
     @Test
     public void Day18A() {
