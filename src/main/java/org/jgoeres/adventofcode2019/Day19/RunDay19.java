bb7da2c13ca7c3be34a7dce2847f829555a1e489
Justin Goeres <jgoeres@liveareacx.com>
2019-12-20 08:10:19 -0500

    Day 19A Add template

---
+++ src/main/java/org/jgoeres/adventofcode2019/Day19/RunDay19.java
@@ -0,0 +1,38 @@
+package org.jgoeres.adventofcode2019.Day19;
+
+public abstract class RunDay19 {
+    static String DAY = "19";
+
+    static String pathToInputs = "data/day" + DAY + "/input.txt";
+
+    static TractorBeamService tractorBeamService = new TractorBeamService();
+
+    public static int problem19A() {
+        // Day 19A
+        System.out.println("=== DAY " + DAY + "A ===");
+
+        int result = 0;
+
+        System.out.println("Day " + DAY + "A: Answer = " + result);
+
+//        Day 19A: Answer =
+//        Time elapsed:	xxx ms
+
+        return result;
+    }
+
+    public static int problem19B() {
+        // Day 19B
+        System.out.println("=== DAY " + DAY + "B ===");
+
+        int result = 0;
+
+        System.out.println("Day " + DAY + "B: Answer = " + result);
+
+//        Day 19B: Answer =
+//        Time elapsed:	xxx ms
+
+        return result;
+    }
+
+}
---
+++ src/test/java/org/jgoeres/adventofcode2019/Day19Test.java
@@ -0,0 +1,31 @@
+package org.jgoeres.adventofcode2019;
+
+import org.jgoeres.adventofcode2019.Day19.RunDay19;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day19Test {
+    static String DAY = "19";
+
+    @Test
+    public void Day19A() {
+        int result = 0;
+        try {
+            result = RunDay19.problem19A();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void Day19B() {
+        int result = 0;
+        try {
+            result = RunDay19.problem19B();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        Assert.assertEquals(0, result);
+    }
+}
